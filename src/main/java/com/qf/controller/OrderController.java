a08f46790c3e50670e8cbea94a0c1e5ac504bf4b
candy-hub <1340744230@qq.com>
2019-10-08 16:02:56 +0800

    配置文件修改-景秋娟提交

--- src/main/java/com/qf/controller/OrderController.java
+++ src/main/java/com/qf/controller/OrderController.java
@@ -1,28 +1,16 @@
 package com.qf.controller;
 
-import com.alibaba.fastjson.JSON;
+
 import com.qf.domain.Cart;
 import com.qf.domain.Orders;
-import com.qf.domain.ShopKinds;
 import com.qf.domain.Shops;
-import com.qf.response.AddShops;
-import com.qf.response.Response;
 import com.qf.service.OrdersService;
-import com.qf.service.ShopKindsService;
-import com.qf.service.ShopsService;
-import com.qf.utils.OrderUtils;
-import com.qf.utils.UploadUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 public class OrderController {
