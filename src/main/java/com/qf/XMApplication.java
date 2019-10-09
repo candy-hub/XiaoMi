6d7547d512a7be9732b1793a8e81d4074c03a4c8
wangqingq <2423668851@qq.com>
2019-10-09 21:22:17 +0800

    部署服务器

--- src/main/java/com/qf/XMApplication.java
+++ src/main/java/com/qf/XMApplication.java
@@ -2,10 +2,17 @@ package com.qf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class XMApplication {
+public class XMApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(XMApplication.class,args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(XMApplication.class);
     }
 }
