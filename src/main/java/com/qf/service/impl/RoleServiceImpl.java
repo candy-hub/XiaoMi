2d36bbee003b351573abf7a1c965d809d1b4e895
candy-hub <1340744230@qq.com>
2019-10-10 19:39:06 +0800

    景秋娟提交

--- src/main/java/com/qf/service/impl/RoleServiceImpl.java
+++ src/main/java/com/qf/service/impl/RoleServiceImpl.java
@@ -1,14 +1,21 @@
 package com.qf.service.impl;
 
 import com.qf.dao.RoleRepository;
+import com.qf.domain.Role;
 import com.qf.service.RoleService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
     @Resource
     private RoleRepository roleRepository;
+
+    @Override
+    public List<Role> findAllRoles() {
+        return roleRepository.findAll();
+    }
 }
