8f3aef58c2db67f62a655b43ea2a637948f91b39
candy-hub <1340744230@qq.com>
2019-10-10 21:46:09 +0800

    景秋娟提交

--- src/main/java/com/qf/service/PermissionService.java
+++ src/main/java/com/qf/service/PermissionService.java
@@ -1,4 +1,17 @@
 package com.qf.service;
 
+import com.qf.domain.Permission;
+
+import java.util.List;
+
 public interface PermissionService {
+
+    List<Permission> findAllPermissions();
+
+    Permission addPermission(Permission permission);
+
+    Permission findPermissionById(int PId);
+
+    Permission updatePermission(Permission permission);
+
 }
